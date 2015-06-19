7ccdc2d9f2b63eb07f3a8663dd32866c7998c2b4
Konstantin Shakhbazov <konstantin.shakhbazov@gmail.com>
2015-06-19 13:29:15 +1000

    test for geno

---
+++ R/test_plink.r
@@ -0,0 +1,27 @@
+library(snpStats)
+geno <- read.plink(bed = "../data/test.bed",
+                   bim = "../data/test.bim",
+                   fam = "../data/test.fam")
+
+my_geno <- as(geno$genotype, "numeric")
+my_geno[1:10,1:10]
+
+mmap_geno <- read.table("../bin/geno.txt", header = FALSE, sep = " ")
+mmap_geno[1:10,1:10]
+na_ind <- which(mmap_geno == 3L, arr.ind = TRUE)
+mmap_geno[na_ind] <- NA
+sum(is.na(my_geno))
+sum(is.na(mmap_geno))
+
+dim(my_geno)
+dim(mmap_geno)
+
+bim <- read.table("../data/test.bim", stringsAsFactors = FALSE)
+
+all.equal(bim$V2, colnames(my_geno))
+2 - mmap_geno[1:10,1:10]
+my_geno[1:10,1:10]
+
+colnames(mmap_geno) <- bim$V2
+
+all.equal(as.matrix(2 - mmap_geno), my_geno, check.attributes = FALSE)
