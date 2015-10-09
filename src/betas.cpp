f01ae3a55de6d169a83cfbaa6714b18825fb7627
Konstantin Shakhbazov <konstantin.shakhbazov@gmail.com>
2015-10-09 10:29:53 +1000

    provide file with non matching alleles

--- src/betas.cpp
+++ src/betas.cpp
@@ -49,29 +49,42 @@ betas::~betas() {
 
 void betas::order_betas(const bim_data& bim) {
    effects_in_order = std::vector<double>(bim.nsnps, 0);
+   std::vector<std::string> snps_with_non_matching_allels;
 
    for(size_t i = 0; i < bim.snps_to_use.size(); i++) {
-      
       if(bim.snps_to_use[i]) {
          
          int beta_index = rs_id2index[ bim.bim_rs_id[i] ];
          effects_in_order[i] = effects[beta_index];
 
          if(bim.bim_ref_all[i] != alleles[beta_index]) {
-            
-            
+                     
             // alternative should be the same as in betas file
             if(bim.bim_another_all[i] != alleles[beta_index]) {
-               throw std::runtime_error("Runtime error: neither reference nor alternative alleles for SNP: " +
-                                        bim.bim_ref_all[i] +
-                                        "agree with the allele provided in the betas file");
+               snps_with_non_matching_allels.push_back(bim.bim_rs_id[i]);
             }
             
             // swap sign for effect size
             effects_in_order[i] = -effects[beta_index];
          }
+
+         
       }
    }
+   
+   if(snps_with_non_matching_allels.size() > 0) {
+
+      std::ofstream non_matching_snp_rs_ids("rs_ids_of_snps_with_non_matching_allels.txt");
+
+      for(size_t j = 0; j < snps_with_non_matching_allels.size(); j++) {
+         non_matching_snp_rs_ids << snps_with_non_matching_allels[j] << "\n";
+      }
+   
+      non_matching_snp_rs_ids.close();
+
+      throw std::runtime_error("Runtime error: bad thing happend");
+    }
+
 }
 
 
